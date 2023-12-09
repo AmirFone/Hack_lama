f692755c88640627290d252e52c8681603bd8d1a
AmirFone <ahossain20@fordham.edu>
2023-12-09 13:55:50 -0500

    adding video

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask, request, redirect, url_for,render_template
+from flask import Flask, request, redirect, url_for,render_template, jsonify
+import os
 
 app = Flask(__name__)
-
-# Dummy user data for demonstration purposes
-users = {"user": "password", "username": "password"}
+curr_video_path=""
+clip_counter=0
+# users = {"user": "password", "username": "password"}
 
 @app.route('/')
 def home():
     return render_template('index.html')  # Render the sign-in page
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    if username in users and users[username] == password:
-        return redirect(url_for('authenticated'))
-    return redirect(url_for('home'))
+# @app.route('/login', methods=['POST'])
+# def login():
+#     username = request.form['username']
+#     password = request.form['password']
+#     if username in users and users[username] == password:
+#         return redirect(url_for('authenticated'))
+#     return redirect(url_for('home'))
 
-@app.route('/authenticated')
-def authenticated():
-    return render_template('demo_upload.html')  # Render the authenticated page
+# @app.route('/authenticated')
+# def authenticated():
+#     return render_template('demo_upload.html')  # Render the authenticated page
+
+@app.route('/video', methods=['POST'])
+def upload_video():
+    global clip_counter
+    if 'video' not in request.files:
+        return jsonify({"error": "No video part"}), 400
+
+    video = request.files['video']
+
+    if video.filename == '':
+        return jsonify({"error": "No selected video"}), 400
+
+    if video:  # If a video is actually present
+        clip_counter+=1
+        video_path = os.path.join('video', f'uploaded_video{clip_counter}.mp4')
+        video.save(video_path)
+        
+        # we return our info
+        return jsonify({"message": "Video uploaded successfully"}), 200
+
+    return jsonify({"error": "Invalid request"}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
